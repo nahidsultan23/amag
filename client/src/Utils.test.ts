70962c13daa3772fa44eff00bfba425f572ecb83
Nahid Sultan <nahid@zaynaxgroup.com>
2021-12-29 11:38:10 +0600

    test cases for time

--- client/src/Utils.test.ts
+++ client/src/Utils.test.ts
@@ -11,9 +11,9 @@ test('timestampToString() in service.ts', () => {
     expect(splittedFirstPortion[0].length).toBe(2);
     expect(splittedFirstPortion[1].length).toBe(2);
     expect(splittedFirstPortion[2].length).toBe(4);
-    expect(splittedFirstPortion[0].length).toBeLessThanOrEqual(31);
-    expect(splittedFirstPortion[1].length).toBeLessThanOrEqual(12);
-    expect(splittedFirstPortion[2].length).toBeLessThanOrEqual(1000);
+    expect(Number(splittedFirstPortion[0])).toBeLessThanOrEqual(31);
+    expect(Number(splittedFirstPortion[1])).toBeLessThanOrEqual(12);
+    expect(Number(splittedFirstPortion[2])).toBeGreaterThanOrEqual(1000);
 
     const trimmedSecondPortion = splittedResult[1].trim();
 
@@ -21,7 +21,10 @@ test('timestampToString() in service.ts', () => {
     expect(splittedSecondPortion[0].length).toBe(2);
     expect(splittedSecondPortion[1].length).toBe(2);
     expect(splittedSecondPortion[2].length).toBe(2);
-    expect(splittedSecondPortion[0].length).toBeLessThanOrEqual(12);
-    expect(splittedSecondPortion[1].length).toBeLessThanOrEqual(59);
-    expect(splittedSecondPortion[2].length).toBeLessThanOrEqual(59);
+    expect(Number(splittedSecondPortion[0])).toBeLessThanOrEqual(12);
+    expect(Number(splittedSecondPortion[1])).toBeLessThanOrEqual(59);
+    expect(Number(splittedSecondPortion[2])).toBeLessThanOrEqual(59);
+
+    const amPm = trimmedSecondPortion.split(' ')[1];
+    expect(amPm === 'AM' || amPm === 'PM').toBe(true);
 });
